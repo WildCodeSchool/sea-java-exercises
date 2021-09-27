3036fe55afc313eecc2dce3a8b099f70204f639d
Alexander C <alexander.culum@gmail.com>
2021-09-27 17:11:00 +0200

    Add hints.

--- src/main/java/quests/fileio/FileHandling.java
+++ src/main/java/quests/fileio/FileHandling.java
@@ -3,6 +3,7 @@ package quests.fileio;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,24 +13,32 @@ public class FileHandling {
         // 1. Read file from new File("src/test/resources/kunden.csv").
         // Use NIO for file reading, the NIO static utility method Files.readAllLines
         // returns a List<String> of all lines in the file, each line one entry of the list
+        File csv = new File("src/test/resources/kunden.csv");
+        List<String> allLines = Files.readAllLines(csv.toPath());
 
-        // 2. Split each line into a String array, use the correct separator string ",".
-        // Each line consists of "id,name,email" of a Kunde.
+        List<Kunde> kundenListe = new ArrayList<>();
+        for (String line : allLines) {
+            // 2. Split each line into a String array, use the correct separator string ",".
+            // Each line consists of "id,name,email" of a Kunde.
+            String[] idAndNameAndMail = line.split(",");
+            // 3. Convert each line into an object of class Kunde, which already has a suitable constructor (id,name,email)
+            Kunde kunde; // add here instantiation of new Kunde(...)
+            // 4. Add all Kunde objects to a new List<Kunde>
+            kundenListe.add(kunde);
+        }
 
-        // 3. Convert each line into an object of class Kunde, which already has a suitable constructor (id,name,email)
+        // 5. In this FileHandling class is a static method searchForId which takes the List<Kunde> and a String searchId, searches for the Kunde with this searchId as ID and returns the name
+        // Implement this method
 
-        // 4. Add all Kunde objects to a new List<Kunde>
-
-        // 5. In this FileHandling class, create a static method searchForId which takes the List<Kunde> and a String searchId, searches for the Kunde with this searchId as ID and returns the name
-
-        // 6. (Opt.) In this class, create a static method sortKunden which takes the List<Kunde>, sorts this list for the names and returns the sorted List<Kunde>
+        // 6. (Opt.) In this FileHandling class is a static method sortKunden which takes the List<Kunde>, sorts this list for the names and returns the sorted List<Kunde>
+        // Implement this method
 
         // 7. (Opt.) Write the sorted list to a new File("src/test/resources/sorted_kunden.csv"), you can use the writeSortedKunden method. To see the file in the IDE you might have to "reload all files from disk".
 
         // 8. Push all your changes to your fork of the repo
     }
 
-    public static String searchForId(List<Kunde> kunden, String id) {
+    public static String searchForId(List<Kunde> kunden, String searchId) {
         // Iterate through the list, search for the id and return the name if found.
         return "THE RESULTING ID";
     }
